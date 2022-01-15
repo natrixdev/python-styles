d332b5191f1cae1d8d1bc18d6c75442c79d935da
natrix_dev <discordlenko@gmail.com>
2022-01-15 12:06:46 +0100

    Add files via upload

---
+++ triangles.py
@@ -0,0 +1,16 @@
+import turtle
+import colorsys
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+t.speed(0)
+n = 50
+h = 0
+for i in range(360) :
+    c = colorsys.hsv_to_rgb(h, 1, 0.8)
+    h+= 1/n
+    t.color(c)
+    t.forward(1*2)
+    t.left(145)
+       
